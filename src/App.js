773c08cfb16be9ffb1c36b148d0fe760d9624584
Caroline Figueira <42359565+Caroline0298@users.noreply.github.com>
2021-09-16 23:27:10 -0300

    react-dio

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () =>{
+  return ( 
+    <>
+  <h1>Minha aplicação com React</h1>
+ <ul>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Item 1</a>
+
+   <li>
+     item 2
+   </li>
+   <li>
+     item 3
+   </li>
+ </ul>
+  </>
+  )
+  
 }
-
 export default App;
